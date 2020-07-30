ed946366fbbee2f77291e5c461a4904663c1b963
Jonas el Fezani Laskemoen <jelaskem@idun-login3.foreman.hpc.ntnu.no>
2020-07-30 17:20:35 +0200

    Adding most important results

--- src/print-to-master.py
+++ src/print-to-master.py
@@ -1,10 +1,15 @@
 import sys
 from src.pretty_print import  print_for_master_thesis_compact, print_for_master_thesis
 
-group_size = 3
-print_folder = f'{sys.argv[1]}/*/'
-print_for_master_thesis(print_folder, ['features', 'layer'], group_size=group_size)
-print_for_master_thesis_compact(print_folder, ['features', 'layer'], group_size=group_size)
+if(len(sys.argv) > 2 and sys.argv[2]):
+    group_size = int(sys.argv[2])
+else:
+    group_size = 3
 
-#print_for_master_thesis(print_folder, ['dropout', 'layer', 'loss'] )
-#print_for_master_thesis_compact(print_folder, ['dropout', 'layer', 'loss'], fields_to_show=['dropout', 'layer', 'loss'], show_model=False)
+
+print_folder = f'{sys.argv[1]}/*/'
+#print_for_master_thesis(print_folder, ['features', 'layer'], group_size=group_size)
+#print_for_master_thesis_compact(print_folder, ['features', 'layer'], group_size=group_size)
+
+print_for_master_thesis(print_folder, ['dropout', 'layer', 'loss'] )
+print_for_master_thesis_compact(print_folder, ['dropout', 'layer', 'loss'], fields_to_show=['dropout', 'layer', 'loss'], show_model=False)
