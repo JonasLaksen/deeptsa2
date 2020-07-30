da226965c5bf03842fb82988cbb7eb2a2f5a472e
Jonas el Fezani Laskemoen <jelaskem@idun-login3.foreman.hpc.ntnu.no>
2020-07-30 21:50:34 +0200

    Legger til resultater fra prisforandring

--- src/print-to-master.py
+++ src/print-to-master.py
@@ -8,8 +8,8 @@ else:
 
 
 print_folder = f'{sys.argv[1]}/*/'
-#print_for_master_thesis(print_folder, ['features', 'layer'], group_size=group_size)
-#print_for_master_thesis_compact(print_folder, ['features', 'layer'], group_size=group_size)
+print_for_master_thesis(print_folder, ['features', 'layer'], group_size=group_size)
+print_for_master_thesis_compact(print_folder, ['features', 'layer'], group_size=group_size)
 
-print_for_master_thesis(print_folder, ['dropout', 'layer', 'loss'] )
-print_for_master_thesis_compact(print_folder, ['dropout', 'layer', 'loss'], fields_to_show=['dropout', 'layer', 'loss'], show_model=False)
+#print_for_master_thesis(print_folder, ['dropout', 'layer', 'loss'] )
+#print_for_master_thesis_compact(print_folder, ['dropout', 'layer', 'loss'], fields_to_show=['dropout', 'layer', 'loss'], show_model=False)
